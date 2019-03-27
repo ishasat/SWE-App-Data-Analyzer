93dbeebc31541e2389474c584440550705ecd367
satpalkar1 <satpalkar.1@osu.edu>
2019-03-27 16:27:25 -0400

    Created schema

---
+++ src/create_member_attendance.sql
@@ -0,0 +1,32 @@
+/* Creates SWE member schema containing USERS, EVENTS and ATTENDANCE tables.
+Author: Isha Satpalkar satpalkar.1@osu.edu
+*/
+CREATE TABLE USERS
+(user_id int not null,
+username text not null,
+officer int not null,
+major text,
+firstName text not null,
+lastName text not null,
+graduationYear int not null,
+primary key(user_id)
+);
+
+CREATE TABLE EVENTS
+(event_id int not null,
+title text not null,
+event_date date not null,
+location text not null,
+description text not null,
+time_range text not null,
+primary key(event_id)
+);
+
+CREATE TABLE ATTENDANCE
+(attendance_id int not null,
+u_id int not null,
+e_id int not null,
+primary key(attendance_id),
+foreign key(u_id) references USERS(user_id),
+foreign key(e_id) references EVENTS(event_id)
+);
